84dcad49cdb1904f3259195f39ca37e199dd1a99
潇湘夜雨 <sang879@sohu.com>
2018-08-24 20:37:41 +0800

    eventbus增加取消监听

--- app/src/main/java/com/eventbus/demo/MainActivity.java
+++ app/src/main/java/com/eventbus/demo/MainActivity.java
@@ -37,5 +37,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Eventbus.getDefault().unregister(this);
     }
 }
